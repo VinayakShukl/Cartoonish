a5d152cd4a8fcdf71b049bbd9a43fffb30aaf79b
JatinSIndhu <jatin11057@iiitd.ac.in>
2014-11-20 12:03:28 +0530

    Edges  Rev. 1

--- Cartoonish/EdgeUtils.cs
+++ Cartoonish/EdgeUtils.cs
@@ -1,5 +1,6 @@
 ﻿using Emgu.CV;
 using Emgu.CV.Structure;
+using System.Drawing;
 
 namespace Cartoonish
 {
@@ -7,7 +8,53 @@ namespace Cartoonish
     {
         public static Image<Bgr, byte> run(Image<Bgr, byte> img)
         {
-            return null;
+            img = img.SmoothMedian(7);
+
+            Image<Gray, byte> canny = img.Convert<Gray,byte>().Canny(20, 150);
+
+            img = canny.Convert<Bgr, byte>();
+
+            img = dilate(img);
+
+            double area = img.Width * img.Height;
+            int threshold = (int)(area * 0.00005);
+
+            img = removeSmallCurves(img, threshold);
+            return img;
+
+        }
+
+        private static Image<Bgr,byte> dilate(Image<Bgr, byte> img)
+        {
+            int[,] tempStructure = { { 1, 1}, {1, 1} };
+            StructuringElementEx element = new StructuringElementEx(tempStructure, 0, 0);
+            CvInvoke.cvDilate(img, img, element, 1);
+            return img;
+        }
+
+
+        private static Image<Bgr,byte> removeSmallCurves(Image<Bgr,byte> img, int threshold)
+        {
+
+            Image<Gray, byte> grayFrame = img.Convert<Gray, byte>();
+            //grayFrame._EqualizeHist();
+
+            var contours = grayFrame.FindContours(Emgu.CV.CvEnum.CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_NONE, Emgu.CV.CvEnum.RETR_TYPE.CV_RETR_LIST);
+
+
+            for (; contours != null; contours = contours.HNext)
+            {
+                Contour<Point> currentContour = contours.ApproxPoly(contours.Perimeter * 0.005);
+
+                if (currentContour.Area < threshold)
+                {
+                    Rectangle currentrect = currentContour.BoundingRectangle;
+
+                    img.Draw(currentrect, new Bgr(0, 0, 0), -1);
+                }
+            }
+            return img;
+
         }
     }
 }
