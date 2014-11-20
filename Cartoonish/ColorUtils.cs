e6bd59390326bad4b08f9e318af2ae402ef82f77
Vinayak Shukl <vinayak11118@iiitd.ac.in>
2014-11-20 14:12:08 +0530

    colors done

--- Cartoonish/ColorUtils.cs
+++ Cartoonish/ColorUtils.cs
@@ -3,6 +3,7 @@ using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.Util;
 using System.Drawing;
+using System;
 
 namespace Cartoonish
 {
@@ -10,27 +11,51 @@ namespace Cartoonish
     {
         public static Image<Bgr, byte> run(Image<Bgr, byte> img){
 
-            Image<Bgr, byte> res = img.Copy();
+            Image<Bgr, byte> small = img.Copy();
             Image<Bgr, byte> temp = img.Copy();
 
-            // down scale
-            double scale = 4;
-            Size smallSize = new Size(img.Size.Width, img.Size.Height);
-
+            // downscale
+            int scale = 4;
+            Size smallSize = new Size(img.Size.Width/scale, img.Size.Height/scale);
+            small.Resize(smallSize.Width, smallSize.Height, INTER.CV_INTER_AREA);
 
             int numReps = 7;
             int kernelSize = 9;
             int colorSigma = 15;
             int spaceSigma = 7;
 
+            // bilatral filtering
             for (int i = 0; i < numReps; i++)
             {
-                temp = res.SmoothBilatral(kernelSize, colorSigma, spaceSigma);
-                res = temp.SmoothBilatral(kernelSize, colorSigma, spaceSigma);
+                temp = small.SmoothBilatral(kernelSize, colorSigma, spaceSigma);
+                small = temp.SmoothBilatral(kernelSize, colorSigma, spaceSigma);
             }
 
-            CvInvoke.cvShowImage("bilatral", res);
-            return res;
+            // upscale
+            small.Resize(img.Width, img.Height, INTER.CV_INTER_AREA);
+            small = small.SmoothMedian(7);
+
+
+            // quantize colors
+            Size size = small.Size;
+            double colorScaleFactor = 24;
+            byte[, ,] data = small.Data;
+
+            for (int row = 0; row < size.Height; row++)
+            {
+                for (int col = 0; col < size.Width; col++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        int pix = (int)data[row, col, i];
+                        pix = (int)(Math.Floor((pix) / colorScaleFactor) * colorScaleFactor);
+                        data[row, col, i] = (byte)pix;
+                    }
+                }
+            }
+
+            small.SmoothMedian(11);
+            return small;
         } 
     }
 }
